b480c54248030ffc6a2bd5813fb44eee4cfc7536
fankunqian <fkq3898885>
2016-08-02 22:04:50 +0800

    update

--- page_build_fns/user_logout.js
+++ page_build_fns/user_logout.js
@@ -2,8 +2,10 @@
  * Created by admin on 16/8/2.
  */
 var user_logout = function(req,res){
-	res.cookie('uid','null',{maxAge:0});
-	res.cookie('token','null',{maxAge:0});
+	//res.cookie('uid','null',{maxAge:0});
+	//res.cookie('token','null',{maxAge:0});
+	res.clearCookie('uid');
+	res.clearCookie('token');
 	var url = global.currenturl;
 	res.redirect(url);
 }
