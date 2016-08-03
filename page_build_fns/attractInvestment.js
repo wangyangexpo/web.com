21e0b7f7683a7e079a9f9a779d496916a9a614fa
fankunqian <fkq3898885>
2016-08-03 13:25:43 +0800

    update

---
+++ page_build_fns/attractInvestment.js
@@ -0,0 +1,16 @@
+/**
+ * Created by admin on 16/8/2.
+ */
+var lib = require('./lib');
+var commeData = require('./commeData');
+
+var attractInvestment = function(req,res){
+	var lib_o = new lib();
+	commeData(req,lib_o,function(count){
+		var content = lib_o.getAllContent();
+		content.shopcart = count;
+		res.render('attractInvestment',content);
+	})
+}
+
+module .exports = attractInvestment
