a38714e2ef5c078336badd111d00cb6169275a42
fankunqian <fkq3898885>
2016-08-08 18:44:24 +0800

    update

--- page_build_fns/appinfo.js
+++ page_build_fns/appinfo.js
@@ -4,21 +4,26 @@
  */
 var lib = require('./lib');
 var commeData = require('./commeData');
-var app = function(req,res,tag){
+var app = function(req, res, tag) {
 	var lib_o = new lib();
 	var _name = req.query.name;
-	lib_o.getData({tag:req.query.tag},'data',false,'/detail')
-		.then(function(){
-			commeData(req,lib_o,function(count){
+	var _tag = req.query.tag;
+	lib_o.getData({
+			tag: _tag
+		}, 'data', false, '/detail')
+		.then(function() {
+			commeData(req, lib_o, function(count) {
 				var content = lib_o.getAllContent(),
+					banner = [{}],
 					data = content.data;
 				var pic_map = [],
 					text_map = [],
 					body_map;
-				if(data&&data.length>0) {
-					data.forEach(function (item) {
+				if(data && data.length > 0) {
+					data.forEach(function(item) {
 						var content_type = item.content_type;
-						switch (content_type) {
+
+						switch(content_type) {
 							case 'pic':
 								pic_map.push(item);
 								break;
@@ -26,24 +31,24 @@ var app = function(req,res,tag){
 								text_map.push(item);
 								break;
 							case 'body':
+								banner[0] = item;
 								body_map = item;
 								break
-						}
-						;
+						};
 					});
 				}
 				var result = {
-					pic_map:pic_map,
-					text_map:text_map,
-					body_map:body_map,
-					cms:content.cms,
+					pic_map: pic_map,
+					text_map: text_map,
+					body_map: body_map,
+					cms: content.cms,
+					banner: banner,
 					title: _name + ' - ' + '应用下载 - ' + content.title,
-					shopcart:count,
-					banner: [{}]
+					shopcart: count
 				}
-				res.render('appinfo',result);
+				res.render('appinfo', result);
 			})
 		})
 }
 
-module .exports = app
+module.exports = app
