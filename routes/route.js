77ebd117c4e9dd9bbe264810fead5e0d90d2421e
fankunqian <fkq3898885>
2016-08-12 19:20:17 +0800

    update

--- routes/route.js
+++ routes/route.js
@@ -6,11 +6,9 @@ var router = express.Router();
 var cookieParser = require('cookie-parser')
 var configs = require('../lib/configs')
 
-var app = express();
-app.use(cookieParser());
-
 router.get('/',(req,res)=>{
-	require('../page_build_fns/index')(req,res);
+	//require('../page_build_fns/index')(req,res);
+	require('../page_build_fns/m_index')(req,res);
 	//if(!req.cookies){
 	//	req.redirect('https://account.ptdev.cn/login?from=mall&callbacl=')
 	//}
@@ -24,7 +22,6 @@ router.get('/brand',(req,res)=>{
 router.get('/app',(req,res)=>{
 	require('../page_build_fns/app')(req,res);
 });
-
 
 router.get('/user/check',(req,res)=>{
 	require('../page_build_fns/user_check')(req,res);
@@ -46,9 +43,10 @@ router.get('/user/logout',(req,res)=>{
 router.get('/brandShow',(req,res)=>{
 	require('../page_build_fns/brandShow')(req,res);
 });
+
 // 联系
 router.get('/contact',(req,res)=>{
-	require('../page_build_fns/contact')(req,res);
+	res.render('contact',{})
 });
 
 // 关于我们
@@ -58,22 +56,22 @@ router.get('/about',(req,res)=>{
 
 // 全国门店
 router.get('/nationalStores',(req,res)=>{
-	require('../page_build_fns/nationalStores')(req,res);
+	res.render('nationalStores',{})
 });
 
 // 合作
 router.get('/cooperation',(req,res)=>{
-	require('../page_build_fns/cooperation')(req,res);
+	res.render('cooperation',{})
 });
 
 // 加入我们
 router.get('/join',(req,res)=>{
-	require('../page_build_fns/join')(req,res);
+	res.render('join',{})
 });
 
 // 品牌招商
 router.get('/attractInvestment',(req,res)=>{
-	require('../page_build_fns/attractInvestment')(req,res);
+	res.render('attractInvestment',{})
 });
 
 // help
@@ -81,8 +79,9 @@ router.get('/help',(req,res)=>{
 	require('../page_build_fns/help')(req,res);
 });
 
-router.get('/appinfo',(req,res)=>{
-	require('../page_build_fns/appinfo')(req,res);
+router.get('/appinfo/:tag',(req,res)=>{
+	var tag = req.params.tag;
+	require('../page_build_fns/appinfo')(req,res,tag);
 })
 router.get('/pt_home',(req,res)=>{
 	require('../page_build_fns/pt_home')(req,res);
@@ -90,41 +89,40 @@ router.get('/pt_home',(req,res)=>{
 
 // 编程
 router.get('/page_hellobc',(req,res)=>{
-	require('../page_build_fns/page_hellobc')(req,res);
+	res.render('hellobc',{})
 })
 // 魔方
 router.get('/page_mofang',(req,res)=>{
-	require('../page_build_fns/page_mofang')(req,res);
+	res.render('mofang',{})
 })
 // 涂涂世界
 router.get('/page_tutushijie',(req,res)=>{
-	require('../page_build_fns/page_tutushijie')(req,res);
+	res.render('tutushijie',{})
 })
 // 麦斯丝
 router.get('/page_maisisi',(req,res)=>{
-	require('../page_build_fns/page_maisisi')(req,res);
+	res.render('maisisi',{})
 })
 // 淘淘向右走
 router.get('/page_taotaoxiangyouzou',(req,res)=>{
-	require('../page_build_fns/page_taotaoxiangyouzou')(req,res);
+	res.render('taotaoxiangyouzou',{})
 })
 // 奇妙发现
 router.get('/page_qimiaofaxian',(req,res)=>{
-	require('../page_build_fns/page_qimiaofaxian')(req,res);
+	res.render('qimiaofaxian',{})
 })
 // 奇妙电路
 router.get('/page_qimiaodianlu',(req,res)=>{
-	require('../page_build_fns/page_qimiaodianlu')(req,res);
+	res.render('qimiaodianlu',{})
 })
 // 哈泥海洋
 router.get('/page_hanihaiyang',(req,res)=>{
-	require('../page_build_fns/page_hanihaiyang')(req,res);
+	res.render('hanihaiyang',{})
 })
 // 底座
 router.get('/page_dizuo',(req,res)=>{
-	require('../page_build_fns/dizuo')(req,res);
+	res.render('dizuo',{})
 })
-
 // 手机端
 router.get('/m_index',(req,res)=>{
 	require('../page_build_fns/m_index')(req,res);
