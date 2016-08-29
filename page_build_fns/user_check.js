e541ea0d56e3831327921565c461f3cd20044b1b
fankunqian <fkq3898885>
2016-08-29 23:40:16 +0800

    update

--- page_build_fns/user_check.js
+++ page_build_fns/user_check.js
@@ -5,7 +5,7 @@ var configs = require('../lib/configs');
 var userCheck = function(req,res){
 	//console.log('nickname:'+req.query);
 	var nickname = req.query.nickname;
-	res.cookie('nickname','nickname',{maxAge: 60*60 * 1000,domain:configs.passport_domain});
+	res.cookie('nickname',nickname,{maxAge: 60*60 * 1000,domain:configs.passport_domain});
 	//global.nickname = nickname;
 	var redirect = req.query.redirect;
 	console.log('nickname:'+nickname+'redirect:'+redirect);
