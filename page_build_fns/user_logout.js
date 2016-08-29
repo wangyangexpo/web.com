1ebf22253af479e6b1f3f890c619385b51c48e57
fankunqian <fkq3898885>
2016-08-29 16:13:24 +0800

    update

--- page_build_fns/user_logout.js
+++ page_build_fns/user_logout.js
@@ -7,7 +7,7 @@ var user_logout = function(req,res){
 	//res.cookie('token','null',{maxAge:0});
 	//res.clearCookie('uid');
 	//res.clearCookie('token');
-	res.cookie('token','0',{domain:configs.passport_domain});
+	res.cookie('uid','0',{domain:configs.passport_domain});
 	//res.cookie('token','0',{domain:configs.passport_domain});
 	//var url = global.currenturl;
 	var url ='';
