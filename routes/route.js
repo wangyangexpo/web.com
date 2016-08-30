7b2c083d9e66860672322364bdc69c83997d58e1
yangzonglong <yangzonglong@admindeMacBook-Pro-101.local>
2016-08-30 18:58:50 +0800

    update

--- routes/route.js
+++ routes/route.js
@@ -192,4 +192,12 @@ router.get('/system/cms/show',(req,res)=>{
 	res.redirect('/help?parent_tag=cms_service&id=653')
 })
 
+router.get('/toys/:id',(req,res)=>{
+	res.redirect('http://store.putao.com/toys/'+req.params.id)
+})
+
+router.get('/ad/:id',(req,res)=>{
+	res.redirect('http://store.putao.com/ad/'+req.params.id)
+})
+
 module .exports = router;
