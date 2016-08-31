ff5d0b3e5f32ede4f50eba0213c7216ed6023b7b
fankunqian <fkq3898885>
2016-08-31 20:14:05 +0800

    Merge branch 'master' of http://code.putao.io/web/putao.com_server
    # Conflicts:
    #	routes/route.js

--- page_build_fns/m_detail.js
+++ page_build_fns/m_detail.js
@@ -11,7 +11,7 @@ var m_detail = function(req, res) {
 		throw new Error('发生错误！', + error);
 	})
 	.then(function() {
-		commeData(req,lib_o,function(count){
+		commeData(req,res,lib_o,function(count){
 			var content = lib_o.getAllContent();
 			content.shopcart = count;
 			content.detail = './' + link;
--- page_build_fns/m_index.js
+++ page_build_fns/m_index.js
@@ -66,7 +66,7 @@ var m_index = function(req, res) {
 		throw new Error('发生错误！', + error);
 	})
 	.then(function() {
-		commeData(req,lib_o,function(count){
+		commeData(req,res,lib_o,function(count){
 			var content = lib_o.getAllContent();
 			content.shopcart = count;
 			// console.log('mobile: '  + JSON.stringify(content));
