62ddca1b6d82b028a800dba6bde49a2f6b6a4156
fankunqian <fkq3898885>
2016-09-24 20:13:15 +0800

    update

--- routes/route.js
+++ routes/route.js
@@ -316,7 +316,7 @@ router.get('/paiband_presale',(req,res)=>{
 	var deviceAgent = req.headers['user-agent'].toLowerCase();
 	var agentID = deviceAgent.match(/(iphone|ipod|ipad|android)/);
 	if(agentID) {
-		res.render('../page_build_fns/m_paiband_presale');
+		require('../page_build_fns/m_paiband_presale')(req,res);
 	}else{
 		res.render('paiband_presale');
 	};
