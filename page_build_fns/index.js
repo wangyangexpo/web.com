4f32d5acacf73036b7aee6b12b4834d397f14ea3
wangyang <183384342@qq.com>
2017-06-13 17:55:21 +0800

    上传到git

--- page_build_fns/index.js
+++ page_build_fns/index.js
@@ -1,18 +1,22 @@
 /**
  * Created by admin on 16/7/22.
  */
-var index = function(req,res){
-	var content = {};
-	content.banners = [
-		{
-			basic_url: 'http://m.topcrab.com/UploadImage/PCIndex/20160718102324439.jpg'
-		},
-		{
-			basic_url: 'http://m.topcrab.com/UploadImage/PCIndex/20160718102411853.jpg'
-		}
-	];
-	content.title = '首页';
-	res.render('index', content);
+var index = function(req, res) {
+    var content = {};
+    content.banners = [
+    	
+        {
+            basic_url: 'http://omw0i6l1a.bkt.clouddn.com/psLogo111.jpg'
+        },
+        {
+            basic_url: 'http://omw0i6l1a.bkt.clouddn.com/timg.jpeg'
+        },
+        {
+            basic_url: 'http://omw0i6l1a.bkt.clouddn.com/timg%20%289%29.jpeg'
+        }
+    ];
+    content.title = '首页';
+    res.render('index', content);
 }
 
-module .exports = index;
+module.exports = index;
